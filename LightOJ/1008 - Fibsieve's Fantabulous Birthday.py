155fef48a59d5b53f3e2bd6ece77dcba62624609
Radoan Sharkar Richi <58891746+radoansharkar@users.noreply.github.com>
2023-02-07 18:04:33 +0600

    Update 1008 - Fibsieve's Fantabulous Birthday.py

--- LightOJ/1008 - Fibsieve's Fantabulous Birthday.py
+++ LightOJ/1008 - Fibsieve's Fantabulous Birthday.py
@@ -7,20 +7,20 @@ for i in range(t):
     n = int(input())
 
     sq = math.ceil(math.sqrt(n))
-    val = (sq*sq-sq)+1
-    if sq%2 == 0:
+    val = (sq * sq - sq) + 1
+    if sq % 2 == 0:
         if n >= val:
             x = sq
-            y = (sq*sq - n)+1
+            y = (sq * sq - n) + 1
         else:
-            x = abs(((sq-1)*(sq-1)+1)-n)+1
+            x = abs(((sq - 1) * (sq - 1) + 1) - n) + 1
             y = sq
     else:
         if n <= val:
             x = sq
-            y = abs(((sq-1)*(sq-1)+1)-n)+1
+            y = abs(((sq - 1) * (sq - 1) + 1) - n) + 1
         else:
-            x = (sq*sq - n)+1
+            x = (sq * sq - n) + 1
             y = sq
     
     print(f"Case {i+1}: {abs(x)} {abs(y)}")
